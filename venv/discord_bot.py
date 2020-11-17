8872b9d0d7526c56dd4ba9a8eace5e60b44c9686
Medjdoub Omar <medjdoub.omar1998@gmail.com>
2020-11-17 12:23:53 +0100

    modifier discord bot code

--- venv/discord_bot.py
+++ venv/discord_bot.py
@@ -4,10 +4,11 @@ import asyncio
 from discord.ext import tasks, commands
 
 
-TOKEN = 'Nzc3NTM2NTcyNzY5MDQyNDMz.X7E3XA.-84GMFS_mJ95tledHneOthhKcBI'
+TOKEN = 'discod bot toker'
 bot = discord.Client()
 @bot.event
 async def on_ready():
+    #you add the id of you channel
     channel = bot.get_channel(777523196387131394)
 
     await channel.send('test')
@@ -19,21 +20,3 @@ bot.run(TOKEN)
 
 
 
-""""
-async def io_related(name):
-    print(f'{name} started')
-    await asyncio.sleep(1)
-    print(f'{name} finished')
-
-
-async def main():
-    await asyncio.gather(
-        io_related('first'),
-        io_related('second'),
-    )  # 1s + 1s = over 1s
-
-
-if __name__ ==  '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-"""
